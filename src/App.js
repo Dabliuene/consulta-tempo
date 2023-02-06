56973370b6c0bc1c5d040da573a9eba5f720dcbf
Nathan Fernandes <walsonathan@gmail.com>
2023-02-06 15:52:04 -0300

    update

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     <div>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
         <a className="navbar-brand text-white" href="#top">
-          Consultar o tempo
+          Previsão do tempo
         </a>
       </nav>
 
